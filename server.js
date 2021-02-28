5d7f125d18ebdb54359c790c96ed7d7527b6dc7f
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-28 17:28:53 -0500

    edited server.js file location and joined CSS using app.use

--- server.js
+++ server.js
@@ -10,11 +10,12 @@ const PORT = 8080;
 // Setting up express app for posting data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "/Develop/public")));
 
 // Setup routes
 
-app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/index.html")));
 
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")));
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/notes.html")));
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
