660626ccd9fda087aa41abac9f63b334fcb7f6a4
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-28 18:31:04 -0500

    saved notes are posting to the side, ned to find a way to delete and render when clicked

--- server.js
+++ server.js
@@ -12,10 +12,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/Develop/public")));
 
-// Setup routes
+const notes = [];
 
+// Setup routes
+// Three GET methods for the index/notes/api routes
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/index.html")));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/notes.html")));
+
+app.get("/api/notes", (req, res) => res.json(notes));
+
+// Post route that takes in a new note and saves it to notes array as JSON object
+app.post("/api/notes", (req, res) => {
+    const newNote = req.body;
+    notes.push(newNote);
+    res.json(newNote);
+});
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
