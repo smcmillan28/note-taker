879e8c6881b8dae6c8e2318eeaf249ba75935308
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-28 21:31:28 -0500

    working to debug appendFile issues

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/Develop/public")));
 
-let rawdata = fs.readFileSync("./Develop/db/db.json");
+const file_name = "./Develop/db/db.json"
+let rawdata = fs.readFileSync(file_name);
 let notes = JSON.parse(rawdata);
 console.log(notes);
 
@@ -31,15 +32,10 @@ app.get("/api/notes", (req, res) => res.json(notes));
 // Post request that takes in a new note and saves it to notes array as JSON object
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
-    fs.appendFile("./Develop/db/db.json", "," + JSON.stringify(newNote), function(err) {
-        if (err) {
-          console.log(err);
-        }
-        else {
-          console.log("New note logged!");
-        }
-      });
-    res.json(newNote);
+    fs.appendFile(file_name, JSON.stringify(newNote), "utf8", function(err, data) {
+        if (err) console.err(err);
+    });
+    res.json(newNote); 
 });
 
 // Delete request that removes note from array when button clicked on notes page
