3481990ee807a9f4b52ff1b496daf10c42ff8bd9
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-28 20:55:48 -0500

    imported fs library to start working with db.json file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // Declaring dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const uniqid = require("uniqid");
 
 // Set up the express app so that it can be hosted on heroku
@@ -13,12 +14,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/Develop/public")));
 
-const notes = [];
+let rawdata = fs.readFileSync("./Develop/db/db.json");
+let notes = JSON.parse(rawdata);
+console.log(notes);
+
 console.log(uniqid());
 
 // Setup routes
 // Three GET requests for the index/notes/api routes
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/index.html")));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/index.html")));
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/Develop/public/notes.html")));
 
@@ -27,7 +31,14 @@ app.get("/api/notes", (req, res) => res.json(notes));
 // Post request that takes in a new note and saves it to notes array as JSON object
 app.post("/api/notes", (req, res) => {
     const newNote = req.body;
-    notes.push(newNote);
+    fs.appendFile("./Develop/db/db.json", "," + JSON.stringify(newNote), function(err) {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("New note logged!");
+        }
+      });
     res.json(newNote);
 });
 
